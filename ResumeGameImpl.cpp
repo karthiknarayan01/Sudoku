436c1cbd23a3cfd33fc3827136fbce071f304df3
karthiknarayan01 <55865439+karthiknarayan01@users.noreply.github.com>
2020-08-15 15:02:04 -0400

    Update ResumeGameImpl.cpp

--- ResumeGameImpl.cpp
+++ ResumeGameImpl.cpp
@@ -1,10 +1,4 @@
-// Final Project : ResumeGameImpl.cpp
-//Author: KETKI MOHIT NAIK , VARSHITHA NALADALA, KARTHIK NARAYAN
-//CPS400.M001.Fall2019.Data Structures and C++ Programming
 
-// Description: ResumeGame implementation
-
-//////////////////////////////////////////////////////////
 
 #include "ResumeGame.h"
 #include <sstream>
