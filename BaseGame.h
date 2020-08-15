fd04fedac0c24a5181057adf5a657173d75ae2bf
karthiknarayan01 <55865439+karthiknarayan01@users.noreply.github.com>
2020-08-15 14:58:38 -0400

    Update BaseGame.h

--- BaseGame.h
+++ BaseGame.h
@@ -1,10 +1,4 @@
-// Final Project : BaseGame.h
-//Author: KETKI MOHIT NAIK , VARSHITHA NALADALA, KARTHIK NARAYAN
-//CPS400.M001.Fall2019.Data Structures and C++ Programming
 
-// Description: BaseGame header file
-
-//////////////////////////////////////////////////////////
 
 #pragma once
 #include "FilledShapes.h"
